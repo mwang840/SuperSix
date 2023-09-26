6aced961f57576013301dd89f0dc581a6f1c611a
jfmarks <124921997+jfmarks@users.noreply.github.com>
2023-09-25 21:05:22 -0400

    added css and fixed the selector function

--- site.js
+++ site.js
@@ -1,12 +1,9 @@
-var sheetIndex = 0;
 var sheets = [
 "/css/AIGenerated.css",
 "/css/pixel.css",
-"/css/pokemon.css",
-"/css/professors.css"];
-var nextSheet = function(){
-    sheetIndex = (sheetIndex + 1) % sheets.length;
-    var newHref = sheets[sheetIndex];
-    document.getElementById('currentstyle').innerHTML = newHref;
-    document.getElementById('pagestyle').setAttribute('href', newHref);
+"/css/professors.css",
+"/css/text.css"];
+var changeSheet = function(style){
+    var newHref = sheets[style];
+    document.getElementById('pagestyle').setAttribute('href', newHref); 
 }
