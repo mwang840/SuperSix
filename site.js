b46d87a1853e732645f750bd7bb856601d936fe7
jfmarks <124921997+jfmarks@users.noreply.github.com>
2023-09-25 21:24:46 -0400

    fixed an issue that caused the css to not be found

--- site.js
+++ site.js
@@ -1,8 +1,8 @@
 var sheets = [
-"/css/AIGenerated.css",
-"/css/pixel.css",
-"/css/professors.css",
-"/css/text.css"];
+"css/AIGenerated.css",
+"css/pixel.css",
+"css/professors.css",
+"css/text.css"];
 var changeSheet = function(style){
     var newHref = sheets[style];
     document.getElementById('pagestyle').setAttribute('href', newHref); 
