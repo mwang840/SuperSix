98d7f560daf442967b1f21ac0d04dcaa50926290
mwang840 <maxwang@udel.edu>
2023-09-27 18:48:48 -0400

    css set up for the accordion for instructions

---
+++ js/toggleInstuctions.js
@@ -0,0 +1,14 @@
+var accordion = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      console.log("Nothing showed");
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
