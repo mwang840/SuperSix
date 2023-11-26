7f96ff1ce8d73bb22a843f3d17e5b4a7a17e971a
mwang840 <maxwang@udel.edu>
2023-11-25 22:09:52 -0500

    changed a bit of things within the files plus fixed some issues

---
+++ js/validateForm.js
@@ -0,0 +1,43 @@
+//grabs the form via the form idea in both register and login files
+let responseForm = document.getElementById("nameform");
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector("form").addEventListener("submit", function(e){
+        e.preventDefault();
+        let email = document.querySelector("#user").value;
+        let password = document.querySelector("#password").value;
+        let id = document.querySelector("#id").value;
+        //^^Grabs the form elements
+        const content = {
+            email: email,
+            password: password,
+            id: id
+        }
+        //Regex for valid emails
+        const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        //Regex for passwords which constin at least 2 special characters and are at LEAST 8 characters long
+        const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+        //Condition if the email isn't valid!
+        if(!emailRegex.test(email)){
+            console.log("Invalid Email");
+            window.alert("Error invalid email");
+            // return false;
+        }
+        else if(!passwordRegex.test(password)){
+            console.log("Invalid Password");
+            window.alert("Error invalid email");
+        }
+        else if(content.email === null || content.password === null || content.id === null){
+            console.log("Cant leave any of those fields empty, try again!");
+            window.alert("Error invalid email");
+        }
+        else{
+            loadGame();
+        }
+    })
+});
+
+function loadGame(){
+    console.log("Game is about to be loaded!");
+    window.location.href = "../boardgame.html"
+}
