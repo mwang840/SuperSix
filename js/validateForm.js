ada89b12d8d100c7cf713a4cca88f4060c53a857
mwang840 <maxwang@udel.edu>
2023-11-28 10:40:02 -0500

    hopefully this works

--- js/validateForm.js
+++ js/validateForm.js
@@ -14,24 +14,28 @@ document.addEventListener("DOMContentLoaded", function(){
             id: id
         }
         //Regex for valid emails
-        const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        //Regex for passwords which constin at least 2 special characters and are at LEAST 8 characters long
+        const emailRegex = /^[A-Za-z0-9._]+@{1}[A-Za-z0-9]+.{1}(com|net|dev|org|edu|gov){1}$/;
+        //Regex for passwords which contains at least 2 special characters and are at LEAST 8 characters long
         const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
         //Condition if the email isn't valid!
         if(!emailRegex.test(email)){
             console.log("Invalid Email");
             window.alert("Error invalid email");
+            e.preventDefault();
             // return false;
         }
         else if(!passwordRegex.test(password)){
             console.log("Invalid Password");
-            window.alert("Error invalid email");
+            e.preventDefault();
+            window.alert("Error invalid password, it must contains at least 2 special characters and are at LEAST 8 characters long");
         }
         else if(content.email === null || content.password === null || content.id === null){
             console.log("Cant leave any of those fields empty, try again!");
+            e.preventDefault();
             window.alert("Error invalid email");
         }
         else if(emailRegex.test(email) && passwordRegex.test(password) && content.email === null && content.password === null && content.id === null){
+            
             loadGame();
         }
     })
