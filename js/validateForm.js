e402af50b354acdf16530dcbf3bcf6bd60d055f4
mwang840 <maxwang@udel.edu>
2023-11-28 11:27:25 -0500

    exhaustive conditionals implemented

--- js/validateForm.js
+++ js/validateForm.js
@@ -17,22 +17,26 @@ document.addEventListener("DOMContentLoaded", function(){
         const emailRegex = /^[A-Za-z0-9._]+@{1}[A-Za-z0-9]+.{1}(com|net|dev|org|edu|gov){1}$/;
         //Regex for passwords which contains at least 2 special characters and are at LEAST 8 characters long
         const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
-        //Condition if the email isn't valid!
-        if(!emailRegex.test(email)){
+        //Condition if the email isn't valid! Exhaustive conditional checks
+        if(emailRegex.test(email) === false && passwordRegex.test(password)===true && (content.email !== null || content.password !== null || content.id !== null)){
             console.log("Invalid Email");
             window.alert("Error invalid email");
+            window.stop();
+            //Prevents the window from being loaded
             e.preventDefault();
             // return false;
         }
-        else if(!passwordRegex.test(password)){
+        else if(emailRegex.test(email) === true && passwordRegex.test(password)===false && (content.email !== null || content.password !== null || content.id !== null)){
             console.log("Invalid Password");
-            e.preventDefault();
             window.alert("Error invalid password, it must contains at least 2 special characters and are at LEAST 8 characters long");
-        }
-        else if(content.email === null || content.password === null || content.id === null){
-            console.log("Cant leave any of those fields empty, try again!");
+            window.stop()
             e.preventDefault();
+        }
+        else if(emailRegex.test(email) === true && passwordRegex.test(password)===true && (content.email === null || content.password === null || content.id == null)){
+            console.log("Cant leave any of those fields empty, try again!");
             window.alert("Error invalid email");
+            window.stop();
+            e.preventDefault();
         }
         else if(emailRegex.test(email) && passwordRegex.test(password) && content.email === null && content.password === null && content.id === null){
             
